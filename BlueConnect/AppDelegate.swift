07dab1e09afa9d09d9b4ff81c0af5d93262b9fd0
grainiac <grainiac@users.noreply.github.com>
2025-11-04 14:16:13 +0100

    Commit after project builds fine for the 1st time

--- BlueConnect/AppDelegate.swift
+++ BlueConnect/AppDelegate.swift
@@ -2,9 +2,6 @@
 //  AppDelegate.swift
 //  BlueConnect
 //
-//  Created by Alex Skoruppa on 31.10.25.
-//
-
 
 import Cocoa
 import CoreAudioKit
--- BlueConnect/main.swift
+++ BlueConnect/main.swift
@@ -2,8 +2,6 @@
 //  main.swift
 //  BlueConnect
 //
-//  Created by Alex Skoruppa on 31.10.25.
-//
 
 import Cocoa
 
